27f04d7df83343e0e79267c5fb93037f74329fa8
kaiesalmahmud <kaies.rmedu03@gmail.com>
2023-06-20 16:59:16 +0600

    add question pairs

--- openai/access_database_test.py
+++ openai/access_database_test.py
@@ -16,8 +16,21 @@ cursor = conn.cursor()
 # query = "SELECT * FROM public.\"monthWise\" LIMIT 50"
 
 # Execute a query to fetch the column names and data types from a table
-query = """SELECT column_name, data_type FROM information_schema.columns WHERE table_schema = 'public' AND table_name = 'monthWise';
-"""
+# query = """SELECT column_name, data_type FROM information_schema.columns WHERE table_schema = 'public' AND table_name = 'monthWise';
+# """
+
+query = """SELECT COALESCE(
+  NULLIF(MIN(NULLIF("Count Device Offline time (hours)", INTERVAL '0 hour')), INTERVAL '0 hour'),
+  MIN(NULLIF("Count Device Offline time (hours)", INTERVAL '0 hour'))
+) AS "Minimum Downtime"
+FROM public."monthWise"
+WHERE "Date" >= DATE_TRUNC('month', CURRENT_DATE);"""
+
+# query = """SELECT SUM(EXTRACT(EPOCH FROM "Count Device Offline time (hours)")/3600) AS "Total Downtime (Hours)"
+# FROM public."monthWise"
+# WHERE "Date" >= DATE_TRUNC('month', CURRENT_DATE)
+# AND "Count Device Offline time (hours)" IS NOT NULL;"""
+
 cursor.execute(query)
 
 # Fetch all the rows returned by the query
--- openai/sql_generator.py
+++ openai/sql_generator.py
@@ -23,6 +23,95 @@ message_history = [{"role": "user", "content": f"""You are a SQL query generator
                     ('Count Device Offline time (hours)', 'interval')
                     ('Remarks', 'text'))
 
+                    Some sample question and query pairs are as follows:
+
+                    Which day had the highest average efficiency?
+                    "
+                    SELECT "Date", AVG("Efficiency %") AS "Total Efficiency"
+                    FROM public."monthWise"
+                    GROUP BY "Date"
+                    ORDER BY "Total Efficiency" DESC
+                    LIMIT 1;
+                    "
+
+
+                    Longest Playtime last month?
+                    "
+                    SELECT "Date",SUM("Total Play time") AS "Longest Playtime"
+                    FROM public."monthWise"
+                    WHERE "Date" >= DATE_TRUNC('month', CURRENT_DATE) - INTERVAL '1 month'
+                    AND "Date" < DATE_TRUNC('month', CURRENT_DATE)
+                    GROUP BY "Date"
+                    ORDER BY "Longest Playtime" DESC
+                    LIMIT 1;
+                    "
+
+
+                    Which day had the Longest Downtime last month?
+                    "
+                    SELECT "Date", "Count Device Offline time (hours)" AS "Longest Downtime"
+                    FROM public."monthWise"
+                    WHERE "Date" >= DATE_TRUNC('month', CURRENT_DATE) - INTERVAL '1 month'
+                    AND "Date" < DATE_TRUNC('month', CURRENT_DATE)
+                    AND "Count Device Offline time (hours)" IS NOT NULL
+                    ORDER BY "Count Device Offline time (hours)" DESC
+                    LIMIT 1;
+                    "
+
+                    Who are the top 5 retailers this month?
+                    "
+                    SELECT "Retail Name", SUM("Efficiency %") AS "Total Efficiency"
+                    FROM public."monthWise"
+                    WHERE "Date" >= DATE_TRUNC('month', CURRENT_DATE)
+                    GROUP BY "Retail Name"
+                    ORDER BY "Total Efficiency" DESC
+                    LIMIT 5;
+                    "
+
+                    Which Zone has the most retailers?
+                    "
+                    SELECT "Zone", COUNT (DISTINCT "Retail Name") AS "Retail Count"
+                    FROM public."monthWise"
+                    GROUP BY "Zone"
+                    ORDER BY "Retail Count" DESC
+                    LIMIT 1;
+                    "
+
+                    Which day of week has the lowest efficiency?
+                    "
+                    SELECT TO_CHAR("Date", 'Day') AS "DayOfWeek", AVG("Efficiency %") AS "Average Efficiency"
+                    FROM public."monthWise"
+                    GROUP BY "DayOfWeek"
+                    ORDER BY "Average Efficiency" ASC
+                    LIMIT 1;
+                    "
+
+                    Which retailers have the most start time after 10am?
+                    "
+                    SELECT "Retail Name", COUNT(*) AS "Start Time Count"
+                    FROM public."monthWise"
+                    WHERE EXTRACT(HOUR FROM "Start Time") >= 10
+                    GROUP BY "Retail Name"
+                    ORDER BY "Start Time Count" DESC;
+                    "
+
+                    What is the highest playtime in Shukrabaad?
+                    "
+                    SELECT MAX("Total Play time") AS "Highest Playtime"
+                    FROM public."monthWise"
+                    WHERE "Location" = 'Shukrabaad';
+                    "
+
+                    At Least how many hours are the tv down this month?
+                    "
+                    SELECT COALESCE(
+                    NULLIF(MIN(NULLIF("Count Device Offline time (hours)", INTERVAL '0 hour')), INTERVAL '0 hour'),
+                    MIN(NULLIF("Count Device Offline time (hours)", INTERVAL '0 hour'))
+                    ) AS "Minimum Downtime"
+                    FROM public."monthWise"
+                    WHERE "Date" >= DATE_TRUNC('month', CURRENT_DATE);
+                    "
+
                     I will ask a quesiton about the data in natural language in my message, and you will reply with a SQL query 
                     that will generate the necessary response when performed on the postgreSQL database. 
                     Reply only with the sql query to further input. If you understand, say OK."""},
