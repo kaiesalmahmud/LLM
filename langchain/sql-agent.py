0314746764dda48ec79d1dbc1dbe0921ff8b3e7e
kaiesalmahmud <kaies.rmedu03@gmail.com>
2023-07-05 18:07:49 +0600

    add SQLDatabaseChain code

--- langchain/sql-agent.py
+++ langchain/sql-agent.py
@@ -1,25 +1,24 @@
+import os
 from langchain.agents import create_sql_agent
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.sql_database import SQLDatabase
 from langchain.chat_models import ChatOpenAI
-import os
 
-# import psycopg2
-
-os.environ["OPENAI_API_KEY"] = open('key.txt', 'r').read().strip()
+API_KEY = open('key.txt', 'r').read().strip()
+os.environ["OPENAI_API_KEY"] = API_KEY
 
 from dotenv import load_dotenv
 load_dotenv()
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo")
-
-host="localhost",
-port="5432",
-database="ReportDB",
-user="postgres",
+host="localhost"
+port="5432"
+database="ReportDB"
+user="postgres"
 password="postgres"
 
-db = SQLDatabase.from_uri("postgresql://postgres:postgres@localhost:5432/ReportDB")
+db = SQLDatabase.from_uri(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo")
 
 toolkit = SQLDatabaseToolkit(db=db, llm=llm)
 
@@ -29,5 +28,5 @@ agent_executor = create_sql_agent(
     verbose=True
 )
 
-agent_executor.run("Who are the top 5 retailers this month?")
+agent_executor.run("Who are the top 5 retailers for the month of May?")
 
--- openai/query-executor.py
+++ openai/query-executor.py
@@ -2,30 +2,36 @@ import psycopg2
 
 # Establish a connection to the PostgreSQL database
 conn = psycopg2.connect(
-    host="adiq-router",
-    port="15432",
+    host="localhost",
+    port="5432",
     database="ReportDB",
     user="postgres",
-    password="postgres1"
+    password="postgres"
 )
 
 # Create a cursor object to interact with the database
 cursor = conn.cursor()
 
 # Execute a SELECT query to fetch the first 50 rows from a table
-# query = "SELECT * FROM public.\"monthWise\" LIMIT 50"
+# query = "SELECT * FROM public.\"monthWise\" LIMIT 5"
 
-query = """SELECT COALESCE(
-  NULLIF(MIN(NULLIF("Count Device Offline time (hours)", INTERVAL '0 hour')), INTERVAL '0 hour'),
-  MIN(NULLIF("Count Device Offline time (hours)", INTERVAL '0 hour'))
-) AS "Minimum Downtime"
-FROM public."monthWise"
-WHERE "Date" >= DATE_TRUNC('month', CURRENT_DATE);"""
+# query = """SELECT COALESCE(
+#   NULLIF(MIN(NULLIF("Count Device Offline time (hours)", INTERVAL '0 hour')), INTERVAL '0 hour'),
+#   MIN(NULLIF("Count Device Offline time (hours)", INTERVAL '0 hour'))
+# ) AS "Minimum Downtime"
+# FROM public."monthWise"
+# WHERE "Date" >= DATE_TRUNC('month', CURRENT_DATE);"""
 
 # query = """SELECT SUM(EXTRACT(EPOCH FROM "Count Device Offline time (hours)")/3600) AS "Total Downtime (Hours)"
 # FROM public."monthWise"
 # WHERE "Date" >= DATE_TRUNC('month', CURRENT_DATE)
 # AND "Count Device Offline time (hours)" IS NOT NULL;"""
+
+query = """SELECT "Retail Name" 
+FROM "monthWise" 
+WHERE EXTRACT(MONTH FROM "Date") = 5 
+ORDER BY "Total Play time" DESC 
+LIMIT 5"""
 
 cursor.execute(query)
 
