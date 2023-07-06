997c92895b2562b82a25b914d0b890c1762473d7
kaiesalmahmud <kaies.rmedu03@gmail.com>
2023-07-06 16:38:45 +0600

    add chat-db codes

---
+++ langchain/chat-db.py
@@ -0,0 +1,136 @@
+import os
+from langchain.agents import *
+from langchain.sql_database import SQLDatabase
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import SQLDatabaseSequentialChain
+from langchain.prompts.prompt import PromptTemplate
+
+import psycopg2
+import pandas as pd
+import streamlit as st
+
+import warnings
+warnings.filterwarnings("ignore")
+
+API_KEY = open('key.txt', 'r').read().strip()
+os.environ["OPENAI_API_KEY"] = API_KEY
+
+from dotenv import load_dotenv
+load_dotenv()
+
+host="localhost"
+port="5432"
+database="ReportDB"
+user="postgres"
+password="postgres"
+
+db = SQLDatabase.from_uri(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+
+
+_postgres_prompt = """You are a PostgreSQL expert. Given an input question, first create a syntactically correct PostgreSQL query to run, then look at the results of the query and return the answer to the input question.
+Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per PostgreSQL. You can order the results to return the most informative data in the database.
+Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
+Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+Pay attention to use CURRENT_DATE function to get the current date, if the question involves "today".
+
+Use the following format:
+
+Question: Question here
+SQLQuery: SQL Query to run
+SQLResult: Result of the SQLQuery
+Answer: Final answer here
+
+"""
+
+PROMPT_SUFFIX = """Only use the following tables:
+{table_info}
+
+Question: {input}"""
+
+POSTGRES_PROMPT = PromptTemplate(
+    input_variables=["input", "table_info", "top_k"],
+    template=_postgres_prompt + PROMPT_SUFFIX,
+)
+
+
+# Setup the database chain
+db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm, database=db, query_prompt=POSTGRES_PROMPT, verbose=False, return_intermediate_steps=True)
+
+def get_query(input_text):
+    result = db_chain(input_text)
+    query = result['intermediate_steps'][1]
+    print(query)
+    return query
+
+# Define function to execute a query on the database and return the results as a dataframe
+def execute_query(query, connection):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    df = pd.DataFrame(results, columns=columns)
+    return df
+
+# Define function to connect to the database using input parameters
+def connect_to_database(host, port, username, password, database):
+    conn = psycopg2.connect(
+        host=host,
+        port=port,
+        database=database,
+        user=username,
+        password=password
+    )
+    return conn
+
+# Create the sidebar for DB connection parameters
+st.sidebar.header("Database Connection")
+host = st.sidebar.text_input("Host", value="localhost")
+port = st.sidebar.text_input("Port", value="5432")
+username = st.sidebar.text_input("Username", value="postgres")
+password = st.sidebar.text_input("Password", value="postgres")
+database = st.sidebar.text_input("Database", value="ReportDB")
+submit_button = st.sidebar.checkbox("Connect")
+
+# Create the main panel
+st.title("Database Query Application")
+
+# Check if the submit button is clicked and establish the database connection
+if submit_button:
+    try:
+        connection = connect_to_database(host, port, user, password, database)
+    except:
+        connection=None
+
+
+    if connection:
+        st.sidebar.success("Connected to the database!")
+    else:
+        st.sidebar.error("Failed to connect to the database. Please check your connection parameters.")
+
+# Get the user's natural question input
+question = st.text_input("Enter your question")
+
+# Create a submit button for executing the query
+query_button = st.button("Submit")
+
+# Execute the query when the submit button is clicked
+if query_button:
+    if connection:
+        # Execute the query and get the results as a dataframe
+        try:
+            query = get_query(question)
+            results_df = execute_query(query, connection)
+        except:
+            st.error("Failed to execute query")
+
+
+        # Display the results as text outputs
+        #st.subheader("Text Outputs")
+        st.text("Refined Question")
+        st.text("Tables Used")
+        st.text("SQL Executed")
+
+        # Display the results as a dataframe
+        st.dataframe(results_df)
--- langchain/db-chain-psyco.py
+++ langchain/db-chain-psyco.py
@@ -5,6 +5,10 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import SQLDatabaseSequentialChain
 from langchain.prompts.prompt import PromptTemplate
 
+import psycopg2
+import pandas as pd
+import streamlit as st
+
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -25,11 +29,6 @@ db = SQLDatabase.from_uri(f"postgresql+psycopg2://{user}:{password}@{host}:{port
 llm = ChatOpenAI(model_name="gpt-3.5-turbo")
 
 
-PROMPT_SUFFIX = """Only use the following tables:
-{table_info}
-
-Question: {input}"""
-
 _postgres_prompt = """You are a PostgreSQL expert. Given an input question, first create a syntactically correct PostgreSQL query to run, then look at the results of the query and return the answer to the input question.
 Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per PostgreSQL. You can order the results to return the most informative data in the database.
 Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
@@ -44,6 +43,11 @@ SQLResult: Result of the SQLQuery
 Answer: Final answer here
 
 """
+
+PROMPT_SUFFIX = """Only use the following tables:
+{table_info}
+
+Question: {input}"""
 
 POSTGRES_PROMPT = PromptTemplate(
     input_variables=["input", "table_info", "top_k"],
@@ -72,10 +76,12 @@ db_chain = SQLDatabaseSequentialChain.from_llm(llm=llm, database=db, query_promp
 
 # get_prompt()
 
-result = db_chain("What the total play time for the month of May?")
+# result = db_chain("What the total play time for the month of May?")
+result = db_chain("list the top 5 retailers")
 
 print('SQL Query: ')
-print(result['intermediate_steps'][1])
+sql_query = result['intermediate_steps'][1]
+print(sql_query)
 print()
 
 print('SQL Result: ')
@@ -84,3 +90,30 @@ print(result['intermediate_steps'][3])
 print()
 print('Answer: ')
 print(result['intermediate_steps'][5])
+
+# Define function to execute a query on the database and return the results as a dataframe
+def execute_query(query, connection):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    df = pd.DataFrame(results, columns=columns)
+    return df
+
+# Define function to connect to the database using input parameters
+def connect_to_database(host, port, username, password, database):
+    conn = psycopg2.connect(
+        host=host,
+        port=port,
+        database=database,
+        user=username,
+        password=password
+    )
+    return conn
+
+connection = connect_to_database(host, port, user, password, database)
+
+result = execute_query(sql_query, connection)
+
+print()
+print(result)
