557871a803282974691c5c7fd61e59ce0efc2993
kaiesalmahmud <kaies.rmedu03@gmail.com>
2023-07-30 17:33:19 +0600

    db-connect experiment

--- langchain/db-connect.py
+++ langchain/db-connect.py
@@ -17,23 +17,6 @@ os.environ["OPENAI_API_KEY"] = API_KEY
 from dotenv import load_dotenv
 load_dotenv()
 
-# host="localhost"
-# port="5432"
-# database="ReportDB"
-# user="postgres"
-# password="postgres"
-
-host="ep-wispy-forest-393400.ap-southeast-1.aws.neon.tech"
-port="5432"
-database="neondb"
-user="db_user"
-password="UdH3VeKu7Cik"
-
-db = SQLDatabase.from_uri(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
-
-llm = ChatOpenAI(model_name="gpt-4", temperature=0)
-
-toolkit = SQLDatabaseToolkit(db=db, llm=llm)
 
 # Implement: If null value is found at the top while trying to sort in descending order, try to look for the next non-null value.
 
@@ -65,57 +48,52 @@ Observation: the result of the action
 Thought: I now know the final answer
 Final Answer: the final answer to the original input question"""
 
-# Define function to connect to the database using input parameters
-def connect_to_database(host, port, username, password, database):
-    connection = psycopg2.connect(
-        host=host,
-        port=port,
-        database=database,
-        user=username,
-        password=password
+def get_response(host, port, database, user, password, input_text):
+
+    db = SQLDatabase.from_uri(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
+
+    llm = ChatOpenAI(model_name="gpt-4", temperature=0)
+
+    toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+    agent_executor = create_sql_agent(
+        llm=llm,
+        toolkit=toolkit,
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        prefix=SQL_PREFIX,
+        suffix=SQL_FUNCTIONS_SUFFIX,
+        format_instructions=FORMAT_INSTRUCTIONS,
+        agent_executor_kwargs = {'return_intermediate_steps': False}
     )
-    return connection
 
-def get_response(input_text):
     response = agent_executor(input_text)
-    sql_query = response['intermediate_steps'][-1][0].tool_input
-    message = response['intermediate_steps'][-1][0].message_log[0].content
     answer = response['output']
-    return sql_query, message, answer
+    return answer
 
-def sqlout(connection,query):
+def get_question(host, port, database, user, password):
 
-    cursor = connection.cursor()
-    cursor.execute(query)
+    # Get the user's natural question input
+    question = st.text_input(":blue[Ask a question:]", placeholder="Enter your question")
 
-    # Given column information
-    column_info = list(cursor.description)
+    # Create a submit button for executing the query
+    query_button = st.button("Submit")
 
-    # Extract column names
-    column_names = [column.name for column in column_info]
+    # Execute the query when the submit button is clicked
+    if query_button:
 
-    # Create a DataFrame
-    df = pd.DataFrame(columns=range(len(column_names)))
+        # Display the results as a dataframe
+        # Execute the query and get the results as a dataframe
+        try:
+            with st.spinner('Calculating...'):
+                answer = get_response(host, port, database, user, password, question)
 
-    # Fetch all the rows returned by the query
-    rows = cursor.fetchall()
-    df=df.from_records(rows).astype(str)
-
-    # Set column names as column headers
-    df.columns = column_names
-    # st.text(query)
-    st.dataframe(df,width=None)
-
-agent_executor = create_sql_agent(
-    llm=llm,
-    toolkit=toolkit,
-    verbose=True,
-    agent_type=AgentType.OPENAI_FUNCTIONS,
-    prefix=SQL_PREFIX,
-    suffix=SQL_FUNCTIONS_SUFFIX,
-    format_instructions=FORMAT_INSTRUCTIONS,
-    agent_executor_kwargs = {'return_intermediate_steps': True}
-)
+            st.subheader("Answer :robot_face:")
+            st.write(answer)
+            # results_df = sqlout(connection, sql_query)
+            st.info(":coffee: _Did that answer your question? If not, try to be more specific._")
+        except:
+            st.warning(":wave: Please enter a valid question. Try to be as specific as possible.")
 
 # Create the sidebar for DB connection parameters
 st.sidebar.header("Choose Database")
@@ -124,7 +102,6 @@ st.sidebar.header("Choose Database")
 st.title("DB Connect :cyclone:")
 st.subheader("Connect your database and ask questions!!")
 
-connection = None
 
 side_button_01 = st.sidebar.button("ReportDB")
 side_button_02 = st.sidebar.button("AccountsDB")
@@ -137,6 +114,8 @@ if side_button_01:
     user="postgres"
     password="postgres"
 
+    get_question(host, port, database, user, password)
+
 elif side_button_02:
 
     host="ep-wispy-forest-393400.ap-southeast-1.aws.neon.tech"
@@ -145,45 +124,7 @@ elif side_button_02:
     user="db_user"
     password="UdH3VeKu7Cik"
 
-
-submit_button = st.sidebar.checkbox("Connect")
-
-# Check if the submit button is clicked and establish the database connection
-if submit_button:
-    try:
-        connection = connect_to_database(host, port, user, password, database)
-    except:
-        connection=None
+    get_question(host, port, database, user, password)
 
 
-    if connection:
-        st.sidebar.success("Connected to the database!")
-    else:
-        st.sidebar.error("Failed to connect to the database. Please check your connection parameters.")
 
-
-# Get the user's natural question input
-question = st.text_input(":blue[Ask a question:]", placeholder="Enter your question")
-
-# Create a submit button for executing the query
-query_button = st.button("Submit")
-
-# Execute the query when the submit button is clicked
-if query_button:
-    if connection:
-
-        # Display the results as a dataframe
-        # Execute the query and get the results as a dataframe
-        try:
-            with st.spinner('Calculating...'):
-                sql_query, message, answer = get_response(question)
-
-            st.subheader("Answer :robot_face:")
-            st.write(answer)
-            # results_df = sqlout(connection, sql_query)
-            st.info(":coffee: _Did that answer your question? If not, try to be more specific._")
-        except:
-            st.warning(":wave: Please enter a valid question. Try to be as specific as possible.")
-
-    else:
-        st.warning(":wave: Please connect to the database first.")
